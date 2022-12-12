4a16312300aa64ba4b13983ecf444d120bb995d0
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-12 11:25:45 -0300

    introducao-while

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
 ﻿int numero = 5;
+int contador = 0;
+
+while(contador <= 10)
+{
+    Console.WriteLine($"{contador + 1}º Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+}
+
+
+/*
+int numero = 5;
 
 for(int contador = 0; contador <= 10; contador++)
 {
    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
 }
+*/
 
